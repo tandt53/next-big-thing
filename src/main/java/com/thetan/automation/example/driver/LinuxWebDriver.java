bb754e828166e9774efe9b2b0987bd43f21097f7
tandt <dothetan.040490@gmail.com>
2017-09-08 16:58:39 +0700

    update abstract class

--- src/main/java/com/thetan/automation/example/driver/LinuxWebDriver.java
+++ src/main/java/com/thetan/automation/example/driver/LinuxWebDriver.java
@@ -1,6 +1,9 @@
 package com.thetan.automation.example.driver;
 
+import com.thetan.automation.example.utils.Constants;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by thetan.do on 12/24/2016.
@@ -9,13 +12,25 @@ public class LinuxWebDriver extends AbstractWebDriver {
     WebDriver driver = null;
 
     public WebDriver getDriver(String browser) {
+        if (browser.equals(Constants.BROWSER_CHROME))
+            chromeDriver();
+        if (browser.equals(Constants.BROWSER_FIREFOX))
+            firefoxDriver();
         return driver;
     }
 
     protected void firefoxDriver() {
+        System.out.println("Windows Firefox Driver");
+        firefoxDriverPath = new String(".//res//driver//lin//geckodriver.exe");
+        System.setProperty(KEY_FIREFOX, firefoxDriverPath);
+        driver = new FirefoxDriver();
     }
 
     protected void chromeDriver() {
+        System.out.println("Windows Chrome Driver");
+        chromeDriverPath = new String(".//res//driver//win//chromedriver.exe");
+        System.setProperty(KEY_CHROME, chromeDriverPath);
+        driver = new ChromeDriver();
     }
 
     protected void edgeDriver() {
