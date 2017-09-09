83e5124b851826a14bfb99228bfa390622e66e5b
tandt53 <dothetan.040490@gmail.com>
2017-09-09 08:03:26 +0700

    update abstract class

--- src/main/java/com/thetan/automation/example/driver/LinuxWebDriver.java
+++ src/main/java/com/thetan/automation/example/driver/LinuxWebDriver.java
@@ -1,26 +1,67 @@
 package com.thetan.automation.example.driver;
 
+import com.thetan.automation.example.utils.Constants;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by thetan.do on 12/24/2016.
  */
 public class LinuxWebDriver extends AbstractWebDriver {
-    WebDriver driver = null;
+    private WebDriver driver = null;
 
     public WebDriver getDriver(String browser) {
+        if (browser.equals(Constants.BROWSER_CHROME))
+            chromeDriver();
+        if (browser.equals(Constants.BROWSER_FIREFOX))
+            firefoxDriver();
         return driver;
     }
 
-    protected void firefoxDriver() {
+    protected void firefoxDriver()  {
+        try {
+            System.out.println("Linux Firefox Driver");
+            File classpathRoot = new File(System.getProperty("user.dir"));
+            File driverDir = new File(classpathRoot, "./res/driver/");
+            File driverExe = new File(driverDir.getCanonicalPath(), "./lin/geckodriver");
+            firefoxDriverPath = driverExe.getAbsolutePath();
+            System.setProperty(KEY_FIREFOX, firefoxDriverPath);
+            driver = new FirefoxDriver();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 
     protected void chromeDriver() {
+        try {
+            System.out.println("L Chrome Driver");
+            File classpathRoot = new File(System.getProperty("user.dir"));
+            File driverDir = new File(classpathRoot, "./res/driver/");
+            File driverExe = new File(driverDir.getCanonicalPath(), "./lin/chromedriver");
+            chromeDriverPath =  driverExe.getAbsolutePath();
+            System.setProperty(KEY_CHROME, chromeDriverPath);
+            driver = new ChromeDriver();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 
     protected void edgeDriver() {
     }
 
     protected void safariDriver() {
+    }
+    protected void androidDriver() {
+
+    }
+
+    protected void iOsDriver() {
+
     }
 }
--- src/main/java/com/thetan/automation/example/utils/Constants.java
+++ src/main/java/com/thetan/automation/example/utils/Constants.java
@@ -7,6 +7,9 @@ public class Constants {
 	public static final String BROWSER_FIREFOX = "FireFox";
 	public static final String BROWSER_IE = "IE";
 	public static final String BROWSER_SAFARI = "Safari";
+	public static final String MOBILE_ANDROID = "Mobile Android";
+	public static final String MOBILE_IOS = "Mobile iOS";
+
 	// link
 	public static final String urlLink = "http://google.com.vn/";
 	public static final String invalidLink = "google.com";
