d46d9ab5b451f73d447dfb60e3f1232db7a13615
tandt53 <dothetan.040490@gmail.com>
2018-07-28 14:17:15 +0700

    update the extent report

--- src/test/java/com/tandt/automation/example/test/HomePageTest.java
+++ src/test/java/com/tandt/automation/example/test/HomePageTest.java
@@ -7,6 +7,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertTrue;
+
 import java.io.IOException;
 
 /**
@@ -14,33 +16,68 @@ import java.io.IOException;
  */
 public class HomePageTest extends BaseTest<HomePageTest> {
 
-    HomePage homePage;
+	HomePage homePage;
 
-    @BeforeTest
-    public void setup() throws IOException {
-        homePage = new HomePage();
-    }
+	@BeforeTest
+	public void setup() throws IOException {
+		homePage = new HomePage();
+	}
 
-    @AfterTest
-    public void teardown() {
-        homePage.close();
-    }
+	@AfterTest
+	public void teardown() {
+		homePage.close();
+	}
 
-    @Test
-    public void checkDriver() {
-        homePage.open()
-                .search("Checking");
-        try {
-			Thread.sleep(5000);
+	@Test
+	public void checkSearch() {
+		homePage.open().search("Checking");
+		assertTrue(false);
+	}
+	
+	@Test
+	public void checkDriver() {
+		homePage.open().search("Checking");
+		try {
+			Thread.sleep(4000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
+	
+	@Test
+	public void checkDriver2() {
+		homePage.open().search("Checking");
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void checkDriver3() {
+		homePage.open().search("Checking");
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void checkDriver4() {
+		homePage.open().search("Checking");
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    @Test
-    public void checkSearch() {
-        homePage.open()
-                .search("Checking");
-    }
+	
+	
 }
