2299fdc55648215935de907bfffb78a01f1cc409
tandt53 <dothetan.040490@gmail.com>
2018-09-25 14:23:26 +0700

    update log

--- src/main/java/com/tandt/automation/example/utils/Log.java
+++ src/main/java/com/tandt/automation/example/utils/Log.java
@@ -1,6 +1,7 @@
 package com.tandt.automation.example.utils;
 
 import org.apache.log4j.Logger;
+import org.testng.ITestResult;
 
 /**
  * Created by thetan.do on 12/28/2016.
@@ -13,7 +14,6 @@ public class Log {
 		Log = Logger.getLogger(clazz);
 	}
 
-	// Need to create these methods, so that they can be called
 	public void info(String message) {
 		Log.info(message);
 	}
@@ -33,28 +33,11 @@ public class Log {
 	public void debug(String message) {
 		Log.debug(message);
 	}
-
-	public void startTestCase(String testName, String description, String params) {
-		Log.info("---------------------------------- Test '" + testName + description
-				+ "' ----------------------------------");
-		Log.info("Started: " + testName);
-		Log.info("Decription: " + description);
-		Log.info("Parameters: " + params);
+	
+	public void error(String[] messages) {
+		for (String msg : messages) {
+			Log.error(msg.toString());
+		}
 	}
 
-	public void endTestCase(String testName, String result) {
-		Log.info("Result: " + result);
-		Log.info("END: " + testName);
-	}
-
-	public void endTestCase(String testName, String result, String testMessage) {
-		Log.info("Result: " + result);
-		Log.info("Message: " + testMessage);
-		Log.info("END: " + testName);
-	}
-
-	public void configurationError(String instanceName, String name, String error) {
-		Log.info("Result CONFIGURATION" + error + "\n");
-		Log.info("END CONFIG " + instanceName + "." + name);
-	}
 }
