33306d8b8974b055e0ef2f3bd8ee944bde8ef2c0
tandt53 <dothetan.040490@gmail.com>
2020-11-02 17:09:12 +0700

    update data provider

---
+++ data-provider/src/main/java/com/tandt53/automation/dataprovider/exceptions/ConditionsException.java
@@ -0,0 +1,8 @@
+package com.tandt53.automation.dataprovider.exceptions;
+
+public class ConditionsException extends Throwable {
+
+    public ConditionsException(String message){
+        super(message);
+    }
+}
