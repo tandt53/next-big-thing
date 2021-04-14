ba4b4e53f01b264fb7db6303e6a39ff814140232
tandt53 <dothetan.040490@gmail.com>
2021-04-14 23:10:24 +0700

    recall common-test

---
+++ common-test/src/main/java/tandt/commontest/model/Meta.java
@@ -0,0 +1,22 @@
+package tandt.commontest.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+
+@Setter
+@Getter
+public class Meta<T extends Meta<?>> {
+
+    private Map<String, T> metas;
+
+    public void add(String key, T value) {
+        metas.put(key, value);
+    }
+
+    public T get(String key){
+        return metas.get(key);
+    }
+
+}
