8ad92e3d1c33c6abce3bbacafdfe0abe9c32f78c
tandt53 <dothetan.040490@gmail.com>
2021-09-19 17:24:12 +0700

    update driver manager

--- data-provider/src/main/java/tandt/dataprovider/json/interfaces/JsonBuilder.java
+++ data-provider/src/main/java/tandt/dataprovider/json/interfaces/JsonBuilder.java
@@ -2,10 +2,9 @@ package tandt.dataprovider.json.interfaces;
 
 public interface JsonBuilder {
 
-    void setFactory(JsonFactory factory);
-
     void addValue(String key, Object value);
 
     void addValueInArray(String key, Object value);
+
 
 }
