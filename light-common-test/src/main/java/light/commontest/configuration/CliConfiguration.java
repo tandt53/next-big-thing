620f8a3e0df45a77d27c2e698bf96e6976b6af64
tandt53 <dothetan.040490@gmail.com>
2022-05-02 12:16:31 +0700

    check pass vars

--- light-common-test/src/main/java/light/commontest/configuration/CliConfiguration.java
+++ light-common-test/src/main/java/light/commontest/configuration/CliConfiguration.java
@@ -11,7 +11,6 @@ public class CliConfiguration extends Configuration {
         Properties properties = System.getProperties();
         Set<String> keys = properties.stringPropertyNames();
         for (String key : keys) {
-            System.out.println(key + "=" + properties.getProperty(key));
             if (key.startsWith(prefix)) {
                 caps.put(key, properties.getProperty(key));
             }
