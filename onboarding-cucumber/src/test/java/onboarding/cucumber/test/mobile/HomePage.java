14be244cf60f924c19f13ab0154bfac370df3767
tandt53 <dothetan.040490@gmail.com>
2022-10-21 22:43:49 +0700

    update actions

--- onboarding-cucumber/src/test/java/onboarding/cucumber/test/mobile/HomePage.java
+++ onboarding-cucumber/src/test/java/onboarding/cucumber/test/mobile/HomePage.java
@@ -1,7 +1,6 @@
 package onboarding.cucumber.test.mobile;
 
 import com.google.inject.Inject;
-import io.cucumber.guice.ScenarioScoped;
 import onboarding.cucumber.test.mobile.components.HomeComponent;
 import onboarding.mobile.page.BasePage;
 import onboarding.mobile.page.PageFactory;
