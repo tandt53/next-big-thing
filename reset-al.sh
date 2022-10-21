f6ef9bd7f62d3ca793f88edec5340b4246cd1f5d
tan <dothetan.040490@gmail.com>
2022-10-21 23:46:23 +0700

    update

--- reset-al.sh
+++
@@ -1,7 +0,0 @@
-git filter-branch --force --commit-filter '
-                GIT_COMMITTER_NAME="tandt53";
-                GIT_AUTHOR_NAME="tandt53";
-                GIT_COMMITTER_EMAIL="dothetan.040490@gmail.com";
-                GIT_AUTHOR_EMAIL="dothetan.040490@gmail.com";
-                git commit-tree "$@";
-        '  --tag-name-filter cat -- --all
