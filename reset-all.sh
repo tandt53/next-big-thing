def8105c96c026a33a9650ad7d9999c0b746b950
tandt53 <dothetan.040490@gmail.com>
2022-10-21 23:17:02 +0700

    reset

--- reset-all.sh
+++ reset-all.sh
@@ -9,18 +9,33 @@
 #    GIT_COMMITTER_EMAIL='dothetan.040490@gmail.com'
 #  " HEAD
 
-git filter-branch --env-filter '
-OLD_EMAIL="v.tandt1@vinid.net"
-CORRECT_NAME="tandt53"
-CORRECT_EMAIL="dothetan.040490@gmail.com"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-fi
-' --tag-name-filter cat -- --branches --tags -f
+#git filter-branch --env-filter '
+#OLD_EMAIL="v.tandt1@vinid.net"
+#CORRECT_NAME="tandt53"
+#CORRECT_EMAIL="dothetan.040490@gmail.com"
+#if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+#then
+#    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+#    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+#fi
+#if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+#then
+#    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+#    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+#fi
+#' --tag-name-filter cat -- --branches --tags -f
+
+git filter-branch --commit-filter '
+        OLD_EMAIL="v.tandt1@vinid.net"
+        CORRECT_NAME="tandt53"
+        CORRECT_EMAIL="dothetan.040490@gmail.com"
+        if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ];
+        then
+                GIT_COMMITTER_NAME="$CORRECT_NAME";
+                GIT_AUTHOR_NAME="#CORRECT_NAME";
+                GIT_COMMITTER_EMAIL="CORRECT_EMAIL";
+                GIT_AUTHOR_EMAIL="CORRECT_EMAIL";
+                git commit-tree "$@";
+        else
+                git commit-tree "$@";
+        fi' HEAD
